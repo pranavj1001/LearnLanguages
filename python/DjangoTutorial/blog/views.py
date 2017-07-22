5e6ba43871a412e8f86aa1d0ae51e47e9275531f
pranavj1001 <pranavj1001@gmail.com>
2017-07-23 01:19:27 +0530

    add new views and templates

--- python/DjangoTutorial/blog/views.py
+++ python/DjangoTutorial/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -6,3 +6,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts' : posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
