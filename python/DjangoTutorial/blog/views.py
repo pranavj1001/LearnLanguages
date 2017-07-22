e0c56d9c65d4751285739ad819c2c14f0b9a3d19
pranavj1001 <pranavj1001@gmail.com>
2017-07-22 22:34:47 +0530

    display the posts on the html file

--- python/DjangoTutorial/blog/views.py
+++ python/DjangoTutorial/blog/views.py
@@ -4,5 +4,5 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {posts})
+    posts = Post.objects.order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts' : posts})
