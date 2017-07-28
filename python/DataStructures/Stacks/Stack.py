a1223fbd7f078ab4289ac4745da1a5f78a892a4f
pranavj1001 <pranavj1001@gmail.com>
2017-07-29 02:37:26 +0530

    add comments

--- python/DataStructures/Stacks/Stack.py
+++ python/DataStructures/Stacks/Stack.py
@@ -1,29 +1,37 @@
 class Stack:
 
+    # constructor
+    # here we initialize the list that will be used for the stack
     def __init__(self):
         self.stack = []
 
+    # method to push element into the stack
     def push(self, data):
         self.stack.append(data)
 
+    # method to pop topmost i.e. last element from the stack
     def pop(self):
         self.stack.pop()
 
+    # method to find whether the stack is empty or not
     def isEmpty(self):
-        if self.stack == []:
+        if not self.stack:
             print("True")
         else:
             print("False")
 
+    # method to find the current length of the stack
     def size(self):
         print(len(self.stack))
 
+    # method to view the top most element
     def viewTop(self):
-        if self.stack != []:
+        if self.stack:
             print(self.stack[len(self.stack) - 1])
         else:
             print("There are no elements in the stack")
 
+    # method to view the full stack
     def viewFullStack(self):
         for i in self.stack:
             print(i)
