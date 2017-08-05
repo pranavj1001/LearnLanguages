7cccf350f5641fa7756efc58b7e0c3c87445e3bf
ganeshmukesh123 <ganeshmukesh123@gmail.com>
2017-08-05 14:27:55 +0530

    Implementing Queue using array and adding some change in Mystack.java

--- java/DataStructure/Array/Mystack.java
+++ java/DataStructure/Array/Mystack.java
@@ -4,24 +4,30 @@ class Stack{
 	private int top;
 
 	Stack(){
-		top=0;
+		top=-1;
 	}
 	void push(int ele){
-		stack[top]=ele;
-		top++;
+		if(top<SIZE){
+			top++;
+			stack[top]=ele;
+		}
+		else{
+			System.out.println("Stack if full");
+		}
 	}
 	int pop(){
-		if(top>0){
+		if(top>=0){
 		    return stack[--top];
 	    } 
 	    else {
+	    	System.out.println("Stack is empty");
 	    	return -1;
 	    } 
 
 	}
 	void display(){
 		System.out.println("Stack is (starting from top)");
-		for(int i=top-1;i>=0;i--){
+		for(int i=top;i>=0;i--){
 			System.out.print(stack[i]+" ");
 		}
 	}
