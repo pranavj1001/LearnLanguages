74970dcc03f22671a3aadfabaa8e57d74b214f2d
pranavj1001 <pranavj1001@gmail.com>
2017-08-10 12:32:59 +0530

    complete the heap data structure

---
+++ python/DataStructures/Heaps/App.py
@@ -0,0 +1,11 @@
+from Heap import Heap
+
+newHeap = Heap()
+
+newHeap.insert(12)
+newHeap.insert(-3)
+newHeap.insert(23)
+newHeap.insert(4)
+
+newHeap.heapSort()
+
--- python/DataStructures/Heaps/Heap.py
+++ python/DataStructures/Heaps/Heap.py
@@ -24,9 +24,51 @@ class Heap(object):
             temp = self.heap[index]
             self.heap[index] = self.heap[parentIndex]
             self.heap[parentIndex] = temp
-
             index = parentIndex
             parentIndex = int((index - 1)/2)
+
+    def fixDown(self, index, upto):
+
+        if upto < 0:
+            upto = self.currentPosition
+
+        while index <= upto:
+            leftChild = 2*index+1
+            rightChild = 2*index+2
+
+            if leftChild <= upto:
+                childToSwap = None
+
+                if rightChild > upto:
+                    childToSwap = leftChild
+                else:
+                    if self.heap[leftChild] > self.heap[rightChild]:
+                        childToSwap = leftChild
+                    else:
+                        childToSwap = rightChild
+
+                if self.heap[index] < self.heap[childToSwap]:
+                    temp = self.heap[index]
+                    self.heap[index] = self.heap[childToSwap]
+                    self.heap[childToSwap] = temp
+                else:
+                    break
+
+                index = childToSwap
+
+            else:
+                break
+
+    #Logic for HeapSort
+    def heapSort(self):
+
+        for i in range(0, self.currentPosition + 1):
+            temp = self.heap[0]
+            print(temp)
+            self.heap[0] = self.heap[self.currentPosition - i]
+            self.heap[self.currentPosition - i] = temp
+            self.fixDown(0, self.currentPosition - i - 1)
+
 
     def isFull(self):
 
@@ -39,6 +81,6 @@ class Heap(object):
         result = self.heap[0]
         self.currentPosition = self.currentPosition - 1
         self.heap[0] = self.heap[self.currentPosition]
-        del self.heap[self.currentPosition]
+        del self.heap[self.currentPosition + 1]
         self.fixDown(0, -1)
         return result
