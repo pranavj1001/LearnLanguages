98e85aa40858dcef6611bf6707ae2d0fbb7521b7
pranavj1001 <pranavj1001@gmail.com>
2019-06-05 16:32:25 +0530

    add macro definition and a round off example

--- Rust/rust.rs
+++ Rust/rust.rs
@@ -1,7 +1,11 @@
 fn main() {
 
     // ------------------ print macros start ------------------
-    // println -> prints statement to console in a new line
+    // Macros: Macros look like functions, except that their name ends with a bang !, 
+    // but instead of generating a function call, macros are expanded into source code 
+    // that gets compiled with the rest of the program. eg. println!
+    // println! -> prints statement to console in a new line
+
     println!("April has {} days.", 31);
     println!("{month} has {numberOfDays} days.", 
         month = "January",
@@ -11,7 +15,11 @@ fn main() {
     println!("{} of {:b} people know binary, the other half doesn't", 1, 2);
     let width = 6;
     println!("{number:>width$} {spaces} spaces and {number}", number = 1, width = width, spaces = width - 1);
-    println!("{}", format!("{:05}", 12)); 
+    println!("{}", format!("{:05}", 12));
+
+    // rounds off the number
+    let pi = 3.141592;
+    println!("Pi is roughly {:.3}", pi);
     // ------------------ print macros end ------------------
 
 }
