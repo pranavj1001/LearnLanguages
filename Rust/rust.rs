1882f9f94eff5c60afcb7441468918c953c31923
pranavj1001 <pranavj1001@gmail.com>
2019-07-13 23:38:42 +0530

    add various simple operations

--- Rust/rust.rs
+++ Rust/rust.rs
@@ -125,6 +125,29 @@ fn main() {
     
     // Variables can be overwritten with shadowing.
     let mutable = true;
+
+    // Integer addition
+    // We need to tell the compiler the type of the literals we use.
+    println!("1 + 2 = {}", 1u32 + 2);
+
+    // Integer subtraction
+    // We need to tell the compiler the type of the literals we use.
+    println!("1 - 2 = {}", 1i32 - 2);
+
+    // Short-circuiting boolean logic
+    println!("true AND false is {}", true && false);
+    println!("true OR false is {}", true || false);
+    println!("NOT true is {}", !true);
+
+    // Bitwise operations
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
+    // Use underscores to improve readability!
+    println!("One million is written as {}", 1_000_000u32);
 }
 // !!!--- end uncommenting from line above ---!!!
 // ------------------ Primitives end ------------------
