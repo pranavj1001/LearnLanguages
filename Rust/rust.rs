87060151cde54aab219e7bdb7049a41166dbe6ed
pranavj1001 <pranavj1001@gmail.com>
2019-08-18 21:24:16 +0530

    add code for tuples

--- Rust/rust.rs
+++ Rust/rust.rs
@@ -101,6 +101,22 @@
 // Rust provides access to a wide variety of primitives.
 
 // !!!--- start uncommenting from line below ---!!!
+use std::fmt;
+
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+impl fmt::Display for Matrix {
+    // `f` is a buffer, this method must write the formatted string into it
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "( {} {} ) \n( {} {} )", self.0, self.1, self.2, self.3)
+    }
+}
+
+fn transpose(matrix: Matrix) -> Matrix {
+    Matrix(matrix.0, matrix.2, matrix.1, matrix.3)
+}
+
 fn main() {
     // Variables can be type annotated.
     let logical: bool = true;
@@ -148,6 +164,43 @@ fn main() {
 
     // Use underscores to improve readability!
     println!("One million is written as {}", 1_000_000u32);
+
+    // A tuple with a bunch of different types
+    let long_tuple = (1u8, 2u16, 3u32, 4u64,
+                      -1i8, -2i16, -3i32, -4i64,
+                      0.1f32, 0.2f64,
+                      'a', true);
+
+    // Values can be extracted from the tuple using tuple indexing
+    println!("long tuple first value: {}", long_tuple.0);
+    println!("long tuple second value: {}", long_tuple.1);
+
+    // Tuples can be tuple members
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+    // Tuples are printable
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+    
+    // But long Tuples cannot be printed
+    // let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+    // println!("too long tuple: {:?}", too_long_tuple);
+    // TODO ^ Uncomment the above 2 lines to see the compiler error
+
+    // To create one element tuples, the comma is required to tell them apart
+    // from a literal surrounded by parentheses
+    println!("one element tuple: {:?}", (5u32,));
+    println!("just an integer: {:?}", (5u32));
+
+    //tuples can be destructured to create bindings
+    let tuple = (1, "hello", 4.5, true);
+
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix);
+    println!("{}", matrix);
+    println!("{}", transpose(matrix));
 }
 // !!!--- end uncommenting from line above ---!!!
 // ------------------ Primitives end ------------------
